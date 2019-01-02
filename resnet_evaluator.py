fixed bug from not restoring batchnorm mean and variance variables

resnet_evaluator.py
@@ -3,6 +3,9 @@ import numpy as np
 import resnet_model
 from os.path import join, basename, dirname
 import utils
+from utils import unitvec_like
+import time
+import shutil
 
 class Evaluator(object):
 
@@ -20,22 +23,22 @@ class Evaluator(object):
                                       spec_coef=0.0,
                                       relu_leakiness=0.1,
                                       projvec_beta=0.0,
-                                      max_grad_norm=30,
+                                      max_grad_norm=30.0,
                                       normalizer='filtnorm',
-                                      specreg_bn=False,
-                                      spec_sign=1,
+                                      specreg_bn=True,
+                                      spec_sign=1.0,
                                       optimizer='mom')
     else:
       self.hps = hps
 
     # model and data loader
-    self.model = resnet_model.ResNet(self.hps, 'eval')
+    self.model = resnet_model.ResNet(self.hps, mode='eval')
     self.model.build_graph()
     self.loader = loader
 
     # session
     self.sess = tf.Session(config=tf.ConfigProto(allow_soft_placement=True, gpu_options=tf.GPUOptions(allow_growth=True)))
-
+    self.sess.run(tf.global_variables_initializer())
 
   def restore_weights(self, log_dir):
 
@@ -54,20 +57,29 @@ class Evaluator(object):
       return True
 
     # restore the checkpoint
-    saver = tf.train.Saver(max_to_keep=1)
+    var_list = list(set(tf.global_variables())-set(tf.global_variables('accum'))-set(tf.global_variables('Sum/projvec')))
+    saver = tf.train.Saver(var_list=var_list, max_to_keep=1)
     saver.restore(self.sess, ckpt_file)
+
+  def restore_weights_dropbox(self, pretrain_dir):
+    utils.load_pretrained(log_dir='/root/ckpt/tmp', pretrain_dir=pretrain_dir)
+    self.restore_weights('/root/ckpt/tmp')
+    shutil.rmtree('/root/ckpt/tmp')
+    print('Ckpt restored from', pretrain_dir)
 
   def assign_weights(self, weights):
     self.sess.run([tf.assign(t,w) for t,w in zip(tf.trainable_variables(), weights)])
+    self.eigval = self.eigvec = self.projvec_corr = None
 
   def get_weights(self):
     return self.sess.run(tf.trainable_variables())
 
-  def eval(self):
+  def eval(self, loader=None):
     # run evaluation session over entire eval set in batches
+    if loader==None: loader = self.loader
     total_prediction, correct_prediction = 0, 0
     running_xent = running_tot = 0
-    for batch_idx, (images, target) in enumerate(self.loader):
+    for batch_idx, (images, target) in enumerate(loader):
       # load batch
       images = images.permute(0,2,3,1).numpy(); target = target.numpy()
       target = np.eye(self.hps.num_classes)[target]
@@ -92,9 +104,38 @@ class Evaluator(object):
   def get_filtnorm(self, weights):
     return self.sess.run(utils.filtnorm(weights))
 
-  def get_hessian(self, loader=None):
+  def get_hessian(self, loader=None, num_classes=10):
     if loader==None: loader = self.loader
-    return utils.hessian_fullbatch(self.sess, self.model, loader, self,hps.num_classes, is_training=False)
+    self.eigval, self.eigvec, self.projvec_corr = \
+      utils.hessian_fullbatch(self.sess, self.model, loader, num_classes, is_training=False)
+    return self.eigval, self.eigvec, self.projvec_corr
+
+  def get_random_dir(self):
+    # create random direction vectors in weight space
+
+    randdir = []
+    weights = self.get_weights()
+    filtnorms = self.get_filtnorm(weights)
+    for l, (layer, layerF) in enumerate(zip(weights, filtnorms)):
+
+      # handle nonconvolutional layers
+      if len(layer.shape)==2: layer = layer[None,None,:,:]; layerF = layerF[None,None,:,:]
+      elif len(layer.shape)!=4: randdir = randdir + [np.zeros(layer.shape)]; continue
+
+      # permute so filter index is first
+      layer = layer.transpose(3,0,1,2)
+      layerF = layerF.transpose(3,0,1,2)
+
+      # make randdir filters that has same norm as the corresponding filter in the weights
+      layerR = np.array([ unitvec_like(filter)*filtnorm for (filter, filtnorm) in zip(layer, layerF) ])
+
+      # permute back to standard
+      layerR = layerR.transpose(1,2,3,0)
+      layerR = np.squeeze(layerR)
+      randdir = randdir + [layerR]
+
+    return randdir
+
 
 
 

resnet_main.py
@@ -93,17 +93,8 @@ def train(hps):
   # set gpu
   os.environ['CUDA_VISIBLE_DEVICES'] = args.gpu
 
-  # load pretrained model from dropbox
-  if args.pretrain_dir:
-    args.pretrain_url = utils.get_dropbox_url(args.pretrain_dir, bin_path=args.bin_path)
-
-  # download pretrained model if a download url was specified
-  print(args.pretrain_url)
-  utils.maybe_download(source_url=args.pretrain_url,
-                       filename=log_dir,
-                       target_directory=None,
-                       filetype='folder',
-                       force=True)
+  # load pretrained model
+  utils.load_pretrained(log_dir, pretrain_dir=args.pretrain_dir)
 
   # build graph [new version]
   cleanloader, dirtyloader, testloader, trainloader = cifar_loader('/root/datasets', batchsize=hps.batch_size, fracdirty=args.fracdirty)
@@ -128,11 +119,11 @@ def train(hps):
   # load checkpoint
   ckpt_state = tf.train.get_checkpoint_state(log_dir)
   if not (ckpt_state and ckpt_state.model_checkpoint_path):
-    tf.logging.info('TRAIN: No pretrained model. Initializing from random')
+    print('TRAIN: No pretrained model. Initializing from random')
     sess.run(tf.global_variables_initializer())
   else:
     saver.restore(sess, ckpt_file)
-    tf.logging.info('TRAIN: Loading checkpoint %s', ckpt_state.model_checkpoint_path)
+    print('TRAIN: Loading checkpoint %s', ckpt_state.model_checkpoint_path)
 
   for epoch in range(args.epoch_end): # loop over epochs
 
@@ -175,22 +166,19 @@ def train(hps):
 
       scheduler.after_run(global_step, len(cleanloader))
 
-      # save ckpt and summary every 100 iters
-      if np.mod(global_step, 100)==0:
-
+      if np.mod(global_step, 250)==0: # record metrics and save ckpt so evaluator can be up to date
         summary_writer.add_summary(summaries, global_step)
         summary_writer.flush()
-        saver.save(sess, ckpt_file)
-        tf.logging.info('TRAIN: loss: %.3f, precision: %.3f, global_step: %d, epoch: %d, time: %s' %
+        print('TRAIN: loss: %.3f, precision: %.3f, global_step: %d, epoch: %d, time: %s' %
                         (loss, prec, global_step, epoch, timenow()))
+        saver.save(sess, ckpt_file)
 
-      # compute spectral radius every 5000 iters
-      if (np.mod(global_step, 1e9)==0 and global_step!=0) or (epoch == args.epoch_end - 1 and batchid == 0):
-        xHx, nextProjvec, corr_iter = utils.hessian_fullbatch(sess, model, cleanloader, hps.num_classes, is_training=True)
+      if np.mod(epoch+1, args.epoch_end) == 0: # compute hessian
+        xHx, nextProjvec, corr_iter = utils.hessian_fullbatch(sess, model, cleanloader, hps.num_classes, is_training=True, num_power_iter=10)
         # compute correlation between projvec of different epochs
         if 'projvec' in locals():
           corr_period = np.sum([np.dot(p.ravel(),n.ravel()) for p,n in zip(projvec, nextProjvec)]) # correlation of projvec of consecutive periods (5000 batches)
-          print('TRAIN: projvec mag', utils.global_norm(projvec), 'nextProjvec mag', utils.global_norm(nextProjvec), 'corr_period', corr_period) # ensure unit magnitude
+          print('HESSIAN: projvec mag', utils.global_norm(projvec), 'nextProjvec mag', utils.global_norm(nextProjvec), 'corr_period', corr_period) # ensure unit magnitude
           experiment.log_metric('corr_period', corr_period, global_step)
         projvec = nextProjvec
         # log hessian results
@@ -219,13 +207,13 @@ def train(hps):
 
 
 
-def evaluate(hps, return_evaluator=False):
+def evaluate(hps):
 
   os.environ['CUDA_VISIBLE_DEVICES'] = '-1' if args.cpu_eval else args.gpu # run eval on cpu
-  _, _, testloader, _ = cifar_loader('/root/datasets', batchsize=hps.batch_size, fracdirty=args.fracdirty)
+  cleanloader, _, testloader, _ = cifar_loader('/root/datasets', batchsize=hps.batch_size, fracdirty=args.fracdirty)
 
   print('===================> EVAL: STARTING SESSION at '+timenow())
-  evaluator = Evaluator(testloader, hps)
+  evaluator = Evaluator(cleanloader, hps)
   print('===================> EVAL: SESSION STARTED at '+timenow()+' on CUDA_VISIBLE_DEVICES='+os.environ['CUDA_VISIBLE_DEVICES'])
 
   # continuously evaluate until process is killed
@@ -250,18 +238,14 @@ def evaluate(hps, return_evaluator=False):
         tag=args.mode + '/Best Precision', simple_value=best_precision)
     summary_writer.add_summary(best_precision_summ, global_step)
     experiment.log_metric('eval/xent', xent, global_step)
-    tf.logging.info('EVAL: loss: %.3f, precision: %.3f, best precision: %.3f time: %s' %
+    print('EVAL: loss: %.3f, precision: %.3f, best precision: %.3f time: %s' %
                     (xent, precision, best_precision, timenow()))
     summary_writer.flush()
 
-    # time.sleep(60)
+    time.sleep(60)
 
-
-# def main(_):
 
 if __name__ == '__main__':
-  tf.logging.set_verbosity(tf.logging.INFO)
-  # tf.app.run()
 
   # set name of experiment and write experiment key into log directory
   experiment.set_name(args.log_root)
@@ -308,6 +292,7 @@ if __name__ == '__main__':
       train(hps)
     finally:
       os.system(join(args.bin_path, 'rek') + ' "mode=eval.*log_root=' + args.log_root + '"') # kill evaluation processes
+      print('killed evaluaton')
 
   elif args.mode == 'eval':
     evaluate(hps)

resnet_model.py
@@ -135,7 +135,6 @@ class ResNet(object):
         self.dirtyNeg = tf.placeholder(name='dirtyNeg', dtype=tf.float32, shape=[None, 10])
         self.dirtyPredictions = self.dirtyOne + self.dirtyNeg * self.predictions
         self.xentPerExample = K.categorical_crossentropy(self.labels, self.dirtyPredictions)
-        # self.xentPerExample = -tf.reduce_sum(self.labels * tf.log(self.dirtyPredictions), reduction_indices=[1])
         self.xent = tf.reduce_mean(self.xentPerExample)
 
     elif self.mode=='eval':
@@ -148,6 +147,10 @@ class ResNet(object):
     # self.xentPerExample = xent # todo oct16 added
     tf.summary.scalar(self.mode+'/xent', self.xent)
 
+    # add spectral radius calculations
+    specreg._spec(self, tf.reduce_sum(self.xentPerExample))
+
+
   def _build_train_op(self):
     """Build training specific ops for the graph."""
 
@@ -156,9 +159,6 @@ class ResNet(object):
 
     # add regularization terms to xent to form loss function
     self.loss = self.xent + self._decay() #+ specreg._spec(self, self.xent)
-
-    # add spectral radius calculations
-    specreg._spec(self, tf.reduce_sum(self.xentPerExample))
 
     # build gradients for training
     trainable_variables = tf.trainable_variables()

specreg.py
@@ -2,22 +2,22 @@ import tensorflow as tf
 import numpy as np
 import utils
 import time
+np.random.seed(1234)
 
 def _spec(net, xent):
   """returns principal eig of the hessian"""
 
   # decide weights from which to compute the spectral radius
+  print('Number of trainable weights: ' + str(utils.count_params(tf.trainable_variables())))
   if not net.hps.specreg_bn: # don't include batch norm weights
     net.regularizable = []
     for var in tf.trainable_variables():
       if var.op.name.find(r'DW') > 0:
         net.regularizable.append(var)
-  else: net.regularizable = tf.trainable_variables() # do include bn weights
-
-  # count number of trainable and regularizable parameters
-  print('Number of trainable weights: ' + str(utils.count_params(tf.trainable_variables())))
-  print('Number of regularizable weights: ' + str(utils.count_params(net.regularizable)))
-  # print('Number of filtnorm elements: ' + str(utils.count_params(net.filtnorm)))
+    print('Number of regularizable weights: ' + str(utils.count_params(net.regularizable)))
+  else:
+    net.regularizable = tf.trainable_variables() # do include bn weights
+    print('Still zeroing out bias and bn variables in hessian calculation')
 
   # create initial projection vector (randomly and normalized)
   projvec_init = [np.random.randn(*r.get_shape().as_list()) for r in net.regularizable]
@@ -33,21 +33,21 @@ def _spec(net, xent):
   # compute filter normalization
   print('normalization scheme: '+net.hps.normalizer)
   if net.hps.normalizer == None or net.hps.normalizer=='None':
-    projvec_normed = net.projvec
+    projvec_mul_normvalues = net.projvec
   else:
     if net.hps.normalizer == 'filtnorm': normalizer = utils.filtnorm
     elif net.hps.normalizer == 'layernorm': normalizer = utils.layernorm
     elif net.hps.normalizer == 'layernormdev': normalizer = utils.layernormdev
-    norm_values = normalizer(net.regularizable)
-    projvec_normed = [tf.multiply(f,p, name='normed/'+p.op.name) for f,p in zip(norm_values, net.projvec)]
+    net.normvalues = normalizer(net.regularizable)
+    projvec_mul_normvalues = [n*p for n,p in zip(net.normvalues, net.projvec)]
 
   # get gradient of loss wrt inputs
   tstart = time.time(); gradLoss = tf.gradients(xent, net.regularizable); print('Built gradLoss: ' + str(time.time() - tstart) + ' s')
 
   # get hessian vector product
-  tstart = time.time();
-  hessVecProd = tf.gradients(gradLoss, net.regularizable, projvec_normed)
-  hessVecProd = [h*n for h,n in zip(hessVecProd, norm_values)]
+  tstart = time.time()
+  hessVecProd = tf.gradients(gradLoss, net.regularizable, projvec_mul_normvalues)
+  hessVecProd = [h*n for h,n in zip(hessVecProd, net.normvalues)]
   print('Built hessVecProd: ' + str(time.time() - tstart) + ' s')
   # tstart = time.time(); hessVecProd = utils.fwd_gradients(gradLoss, net.regularizable, projvec_filtnorm); print('Built hessVecProd: ' + str(time.time() - tstart) + ' s')
 
@@ -55,7 +55,7 @@ def _spec(net, xent):
   with tf.variable_scope('accum'):
     hessvecprodAccum = [tf.Variable(tf.zeros_like(h), trainable=False, name=h.op.name) for h in hessVecProd]
     net.zero_op = [a.assign(tf.zeros_like(a)) for a in hessvecprodAccum]
-    net.accum_op = [a.assign_add(g)/50000 for a,g in zip(hessvecprodAccum, hessVecProd)]
+    net.accum_op = [a.assign_add(g/50000) for a,g in zip(hessvecprodAccum, hessVecProd)]
 
   # principal eigenvalue: project hessian-vector product with that same vector
   net.xHx = utils.list2dotprod(net.projvec, hessvecprodAccum)
@@ -75,13 +75,13 @@ def _spec(net, xent):
   net.projvec_dist = utils.list2euclidean(nextProjvec, net.projvec)
 
   # op to assign the new projection vector for next iteration
-  with tf.control_dependencies([net.projvec_corr, net.projvec_dist]):
+  with tf.control_dependencies([net.projvec_corr, net.projvec_dist, net.xHx]):
     with tf.variable_scope('projvec_op'):
       net.projvec_op = [tf.assign(p,n) for p,n in zip(net.projvec, nextProjvec)]
 
   # spectral penalty: multiply by spectral radius coefficient and return
   net.spec_coef = tf.constant(0, tf.float32)
-  spec_penalty = tf.multiply(net.hps.spec_sign, tf.multiply(net.spec_coef, tf.maximum(net.xHx, 0)))
+  spec_penalty = tf.multiply(net.hps.spec_sign, tf.multiply(net.spec_coef, tf.maximum(net.xHx, 0.0)))
 
   return spec_penalty
 

surface.py
@@ -20,65 +20,31 @@ else:
   comet_key = open('comet_expt_key.txt', 'r').read()
   experiment = ExistingExperiment(api_key="vPCPPZrcrUBitgoQkvzxdsh9k", previous_experiment=comet_key, parse_args=False)
 
-
 np.random.seed(1239)
-os.environ['CUDA_VISIBLE_DEVICES'] = '1'
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 cleanloader, _, _, _ = cifar_loader('/root/datasets', batchsize=2*512, fracdirty=.5)
 evaluator = Evaluator(cleanloader)
-evaluator.restore_weights('/root/ckpt/poison-strong')
+evaluator.restore_weights_dropbox('ckpt/poison-filtnorm-weaker')
+evaluator.eval()
 weights = evaluator.get_weights()
 
-# # do some statistics on weights and plot histogram
-# weightsPermuted = [w.transpose(3,0,1,2) if len(w.shape)>2 else w[None,None,:,:].transpose(3,0,1,2) if len(w.shape)==2 else w for w in weights]
-# filter2norm = lambda filters: [np.linalg.norm(f.ravel()) for f in filters]
-# filternorms = [filter2norm(filters) if len(filters.shape) > 1 else None for filters in weightsPermuted]
-# filternorms = reduce(lambda running_norms, norms: running_norms + norms if norms != None else running_norms, filternorms)
-# hist(filternorms, 20); show()
-
-def get_random_dir(weights):
-  # create random direction vectors in weight space
-
-  randdir = []
-  filtnorms = evaluator.get_filtnorm(weights)
-  for l, (layer, layerF) in enumerate(zip(weights, filtnorms)):
-
-    # handle nonconvolutional layers
-    if len(layer.shape)==2: layer = layer[None,None,:,:]; layerF = layerF[None,None,:,:]
-    elif len(layer.shape)!=4: randdir = randdir + [np.zeros(layer.shape)]; continue
-
-    # permute so filter index is first
-    layer = layer.transpose(3,0,1,2)
-    layerF = layerF.transpose(3,0,1,2)
-
-    # make randdir filters that has same norm as the corresponding filter in the weights
-    layerR = np.array([ unitvec_like(filter)*filtnorm for (filter, filtnorm) in zip(layer, layerF) ])
-
-    # permute back to standard
-    layerR = layerR.transpose(1,2,3,0)
-    layerR = np.squeeze(layerR)
-    randdir = randdir + [layerR]
-
-  return randdir
-
-dw1 = get_random_dir(weights)
-dw2 = get_random_dir(weights)
-
 eigval, dw1, projvec_corr = evaluator.get_hessian()
+# dw1 = evaluator.get_random_dir()
+dw2 = evaluator.get_random_dir()
 
-cfeed = .5 * np.linspace(-1, 1, 5)
+cfeed = .5 * np.linspace(-1, 1, 100)
 
 xent = np.zeros(len(cfeed))
 for i, c in enumerate(cfeed):
 
-  perturbedWeights = [w + c * d1 for w, d1 in zip(weights, dw1)]
+  perturbedWeights = [w + c * d1 for w, d1 in zip(weights, dw2)]
   evaluator.assign_weights(perturbedWeights)
   xent[i], acc, _ = evaluator.eval()
   print('progress:', i+1, 'of', len(cfeed), '| time:', time())
 
 xent = np.reshape(np.array(xent), cfeed.shape)
 plt.plot(cfeed, xent)
-plt.savefig('for_comet.jpg')
-experiment.log_image('for_comet.jpg')
+experiment.log_figure('landscape')
 
 # clin = 1.5 * np.linspace(-1, 1,40)
 # cc1, cc2 = np.meshgrid(clin, clin)

utils.py
@@ -67,7 +67,8 @@ def filtnorm(trainable_variables):
           f.append(tf.multiply(tf.ones_like(r[:,i]), tf.norm(r[:,i])))
         filtnorm.append(tf.stack(f,axis=1))
       elif len(r.shape)==1: # bn and bias layer
-        f = tf.multiply(tf.ones_like(r), tf.norm(r))
+        # f = tf.multiply(tf.ones_like(r), tf.norm(r)) # do not do any normalization/scaling to bias/bn variables
+        f = tf.multiply(tf.zeros_like(r), tf.norm(r)) # zero out bias/bn variables so their curvature doesnt affect hessian
         filtnorm.append(f)
       else:
         print('invalid number of dimensions in layer, should be 1, 2, or 4')
@@ -89,7 +90,7 @@ def filtnormbyN(trainable_variables):
   return [tf.divide(f, tf.cast(c, dtype=tf.float32)) for c,f in zip(filtcnt, norm_values)]
 
 
-def hessian_fullbatch(sess, model, loader, num_classes=10, is_training=False, num_power_iter=6):
+def hessian_fullbatch(sess, model, loader, num_classes=10, is_training=False, num_power_iter=20):
   '''compute fullbatch hessian eigenvalue/eigenvector given an image loader and model'''
 
   for power_iter in range(num_power_iter): # do power iteration to find spectral radius
@@ -111,9 +112,11 @@ def hessian_fullbatch(sess, model, loader, num_classes=10, is_training=False, nu
       else:
         sess.run(model.accum_op, {model._images: batchimages, model.labels: batchtarget})
 
+      if power_iter==0 and bid==0: print('Starting hessian calculation, just did first batch of first power iteration')
+
     # calculated projected hessian eigvec and eigval
-    projvec_op, projvec_corr, xHx, projvec = sess.run([model.projvec_op, model.projvec_corr, model.xHx, model.projvec])
-    print('HESSIAN: power_iter', power_iter, 'xHx', xHx, 'projvec_corr', projvec_corr, 'elapsed', time.time()-tstart)
+    projvec_op, projvec_corr, xHx, projvec, normvalues = sess.run([model.projvec_op, model.projvec_corr, model.xHx, model.projvec, model.normvalues])
+    print('HESSIAN: power_iter', power_iter, 'xHx', xHx, 'projvec_corr', projvec_corr, 'projvec_magnitude', global_norm(projvec), 'elapsed', time.time()-tstart)
 
   return xHx, projvec, projvec_corr
 
@@ -227,3 +230,18 @@ class Scheduler(object):
 
 def timenow():
   return datetime.now().strftime('%m-%d %H:%M:%S')
+
+# load pretrained model from dropbox
+def load_pretrained(log_dir, pretrain_dir=None, pretrain_url=None, bin_path=''):
+
+  if pretrain_dir:
+    pretrain_url = get_dropbox_url(pretrain_dir, bin_path=bin_path)
+
+  # download pretrained model if a download url was specified
+  print(pretrain_url)
+  maybe_download(source_url=pretrain_url,
+                 filename=log_dir,
+                 target_directory=None,
+                 filetype='folder',
+                 force=True)
+