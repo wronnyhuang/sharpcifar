75a67e93cc9921657f808c3955fa430e06b7708b
tom <tomg@cs.umd.edu>
2019-03-20 05:31:53 -1000

    launch scripts

---
+++ launch/pbs_scripts/sharp13_qsub_sOfX9PgY6.sh
@@ -0,0 +1,79 @@
+
+#####################################
+#   PBS script auto generated
+#   by pbs_job_launch.py
+#####################################
+#!/bin/sh -f
+#$ -cwd
+#PBS -N sharp13
+#PBS -o /gpfs/scratch/tomg/sharpcifar/launch/pbs_scripts/sharp13_output_sOfX9PgY6.txt
+#PBS -j oe
+#PBS -l walltime=20:3:0
+#PBS -A MHPCC96670DA1
+#PBS -l select=1:mpiprocs=1:ncpus=20
+#PBS -q standard
+#####################################
+echo "-----------------command run by pbs_job_launch.py-----------------------"
+echo "
+    python surface2d.py -name=svhn_clean  -gpu=0 -part=24 -npart=32 -url=https://www.dropbox.com/sh/v1unz2j931fsmkr/AADVteaZ51B5Xy0eEsMzlgDWa?dl=0  &> log/console_gpu=0_job=13.out &
+    pid0=$!
+    python surface2d.py -name=svhn_clean  -gpu=1 -part=25 -npart=32 -url=https://www.dropbox.com/sh/v1unz2j931fsmkr/AADVteaZ51B5Xy0eEsMzlgDWa?dl=0  &> log/console_gpu=1_job=13.out &
+    pid1=$!
+    python surface2d.py -name=svhn_clean  -gpu=2 -part=26 -npart=32 -url=https://www.dropbox.com/sh/v1unz2j931fsmkr/AADVteaZ51B5Xy0eEsMzlgDWa?dl=0  &> log/console_gpu=2_job=13.out &
+    pid2=$!
+    python surface2d.py -name=svhn_clean  -gpu=3 -part=27 -npart=32 -url=https://www.dropbox.com/sh/v1unz2j931fsmkr/AADVteaZ51B5Xy0eEsMzlgDWa?dl=0  &> log/console_gpu=3_job=13.out &
+    pid3=$!
+    wait $pid0
+    wait $pid1
+    wait $pid2
+    wait $pid3
+    "
+echo "---------------------------pbs options----------------------------------"
+echo PBS -l select=1:mpiprocs=1:ncpus=20
+echo PBS -l walltime=20:3:0
+echo PBS -A MHPCC96670DA1
+echo PBS -q standard
+echo "------------------------------------------------------------------------"
+echo "Job started on" `date`
+echo "------------------------------------------------------------------------"
+echo "
+cdw
+cd /gpfs/scratch/tomg/sharpcifar
+"
+
+cdw
+cd /gpfs/scratch/tomg/sharpcifar
+
+echo "
+    python surface2d.py -name=svhn_clean  -gpu=0 -part=24 -npart=32 -url=https://www.dropbox.com/sh/v1unz2j931fsmkr/AADVteaZ51B5Xy0eEsMzlgDWa?dl=0  &> log/console_gpu=0_job=13.out &
+    pid0=$!
+    python surface2d.py -name=svhn_clean  -gpu=1 -part=25 -npart=32 -url=https://www.dropbox.com/sh/v1unz2j931fsmkr/AADVteaZ51B5Xy0eEsMzlgDWa?dl=0  &> log/console_gpu=1_job=13.out &
+    pid1=$!
+    python surface2d.py -name=svhn_clean  -gpu=2 -part=26 -npart=32 -url=https://www.dropbox.com/sh/v1unz2j931fsmkr/AADVteaZ51B5Xy0eEsMzlgDWa?dl=0  &> log/console_gpu=2_job=13.out &
+    pid2=$!
+    python surface2d.py -name=svhn_clean  -gpu=3 -part=27 -npart=32 -url=https://www.dropbox.com/sh/v1unz2j931fsmkr/AADVteaZ51B5Xy0eEsMzlgDWa?dl=0  &> log/console_gpu=3_job=13.out &
+    pid3=$!
+    wait $pid0
+    wait $pid1
+    wait $pid2
+    wait $pid3
+    "
+
+    python surface2d.py -name=svhn_clean  -gpu=0 -part=24 -npart=32 -url=https://www.dropbox.com/sh/v1unz2j931fsmkr/AADVteaZ51B5Xy0eEsMzlgDWa?dl=0  &> log/console_gpu=0_job=13.out &
+    pid0=$!
+    python surface2d.py -name=svhn_clean  -gpu=1 -part=25 -npart=32 -url=https://www.dropbox.com/sh/v1unz2j931fsmkr/AADVteaZ51B5Xy0eEsMzlgDWa?dl=0  &> log/console_gpu=1_job=13.out &
+    pid1=$!
+    python surface2d.py -name=svhn_clean  -gpu=2 -part=26 -npart=32 -url=https://www.dropbox.com/sh/v1unz2j931fsmkr/AADVteaZ51B5Xy0eEsMzlgDWa?dl=0  &> log/console_gpu=2_job=13.out &
+    pid2=$!
+    python surface2d.py -name=svhn_clean  -gpu=3 -part=27 -npart=32 -url=https://www.dropbox.com/sh/v1unz2j931fsmkr/AADVteaZ51B5Xy0eEsMzlgDWa?dl=0  &> log/console_gpu=3_job=13.out &
+    pid3=$!
+    wait $pid0
+    wait $pid1
+    wait $pid2
+    wait $pid3
+    
+echo "------------------------------------------------------------------------"
+echo "Job ended on" `date`
+echo "------------------------------------------------------------------------"
+
+exit
---
+++ launch/pbs_scripts/sharp15_qsub_aiGPuNOm9.sh
@@ -0,0 +1,79 @@
+
+#####################################
+#   PBS script auto generated
+#   by pbs_job_launch.py
+#####################################
+#!/bin/sh -f
+#$ -cwd
+#PBS -N sharp15
+#PBS -o /gpfs/scratch/tomg/sharpcifar/launch/pbs_scripts/sharp15_output_aiGPuNOm9.txt
+#PBS -j oe
+#PBS -l walltime=20:3:0
+#PBS -A MHPCC96670DA1
+#PBS -l select=1:mpiprocs=1:ncpus=20
+#PBS -q standard
+#####################################
+echo "-----------------command run by pbs_job_launch.py-----------------------"
+echo "
+    python surface2d.py -name=svhn_clean  -gpu=0 -part=28 -npart=32 -url=https://www.dropbox.com/sh/v1unz2j931fsmkr/AADVteaZ51B5Xy0eEsMzlgDWa?dl=0  &> log/console_gpu=0_job=15.out &
+    pid0=$!
+    python surface2d.py -name=svhn_clean  -gpu=1 -part=29 -npart=32 -url=https://www.dropbox.com/sh/v1unz2j931fsmkr/AADVteaZ51B5Xy0eEsMzlgDWa?dl=0  &> log/console_gpu=1_job=15.out &
+    pid1=$!
+    python surface2d.py -name=svhn_clean  -gpu=2 -part=30 -npart=32 -url=https://www.dropbox.com/sh/v1unz2j931fsmkr/AADVteaZ51B5Xy0eEsMzlgDWa?dl=0  &> log/console_gpu=2_job=15.out &
+    pid2=$!
+    python surface2d.py -name=svhn_clean  -gpu=3 -part=31 -npart=32 -url=https://www.dropbox.com/sh/v1unz2j931fsmkr/AADVteaZ51B5Xy0eEsMzlgDWa?dl=0  &> log/console_gpu=3_job=15.out &
+    pid3=$!
+    wait $pid0
+    wait $pid1
+    wait $pid2
+    wait $pid3
+    "
+echo "---------------------------pbs options----------------------------------"
+echo PBS -l select=1:mpiprocs=1:ncpus=20
+echo PBS -l walltime=20:3:0
+echo PBS -A MHPCC96670DA1
+echo PBS -q standard
+echo "------------------------------------------------------------------------"
+echo "Job started on" `date`
+echo "------------------------------------------------------------------------"
+echo "
+cdw
+cd /gpfs/scratch/tomg/sharpcifar
+"
+
+cdw
+cd /gpfs/scratch/tomg/sharpcifar
+
+echo "
+    python surface2d.py -name=svhn_clean  -gpu=0 -part=28 -npart=32 -url=https://www.dropbox.com/sh/v1unz2j931fsmkr/AADVteaZ51B5Xy0eEsMzlgDWa?dl=0  &> log/console_gpu=0_job=15.out &
+    pid0=$!
+    python surface2d.py -name=svhn_clean  -gpu=1 -part=29 -npart=32 -url=https://www.dropbox.com/sh/v1unz2j931fsmkr/AADVteaZ51B5Xy0eEsMzlgDWa?dl=0  &> log/console_gpu=1_job=15.out &
+    pid1=$!
+    python surface2d.py -name=svhn_clean  -gpu=2 -part=30 -npart=32 -url=https://www.dropbox.com/sh/v1unz2j931fsmkr/AADVteaZ51B5Xy0eEsMzlgDWa?dl=0  &> log/console_gpu=2_job=15.out &
+    pid2=$!
+    python surface2d.py -name=svhn_clean  -gpu=3 -part=31 -npart=32 -url=https://www.dropbox.com/sh/v1unz2j931fsmkr/AADVteaZ51B5Xy0eEsMzlgDWa?dl=0  &> log/console_gpu=3_job=15.out &
+    pid3=$!
+    wait $pid0
+    wait $pid1
+    wait $pid2
+    wait $pid3
+    "
+
+    python surface2d.py -name=svhn_clean  -gpu=0 -part=28 -npart=32 -url=https://www.dropbox.com/sh/v1unz2j931fsmkr/AADVteaZ51B5Xy0eEsMzlgDWa?dl=0  &> log/console_gpu=0_job=15.out &
+    pid0=$!
+    python surface2d.py -name=svhn_clean  -gpu=1 -part=29 -npart=32 -url=https://www.dropbox.com/sh/v1unz2j931fsmkr/AADVteaZ51B5Xy0eEsMzlgDWa?dl=0  &> log/console_gpu=1_job=15.out &
+    pid1=$!
+    python surface2d.py -name=svhn_clean  -gpu=2 -part=30 -npart=32 -url=https://www.dropbox.com/sh/v1unz2j931fsmkr/AADVteaZ51B5Xy0eEsMzlgDWa?dl=0  &> log/console_gpu=2_job=15.out &
+    pid2=$!
+    python surface2d.py -name=svhn_clean  -gpu=3 -part=31 -npart=32 -url=https://www.dropbox.com/sh/v1unz2j931fsmkr/AADVteaZ51B5Xy0eEsMzlgDWa?dl=0  &> log/console_gpu=3_job=15.out &
+    pid3=$!
+    wait $pid0
+    wait $pid1
+    wait $pid2
+    wait $pid3
+    
+echo "------------------------------------------------------------------------"
+echo "Job ended on" `date`
+echo "------------------------------------------------------------------------"
+
+exit
